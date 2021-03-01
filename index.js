26448b7a1bb761bac11b9eba8e0507145c1fea78
Pronounced <jokerknight670@gmail.com>
2021-03-01 11:39:18 -0600

    removed unnecessary log.

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const quiz = function()
     console.log('QUIZ')
     console.log(ans = question('1. What is 3 times 4?'));
     if(ans === '12' || 'twelve') score++;
-    console.log(ans);
 
     console.log(ans = question('2.How many legs does a cat have?'));
     if(ans === '4' || 'four') score++;
@@ -20,7 +19,7 @@ const quiz = function()
     console.log(ans = question('4. What color does mixing blue and yellow create?'));
     if(ans === 'green') score++;
 
-    console.log('Score: ' + score)
+    console.log('Score: ${score}');
 }
 
 quiz();
